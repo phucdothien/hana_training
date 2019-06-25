9a0005ae7017f9ad574c04ca2826a2aed6bed695
phucdothien <phucdo@saptech.edu.vn>
2019-06-25 11:20:09 +0700

    First commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ZDEMO_C_SALESORDERdemo_sales_order.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ZDEMO_C_SALESORDERdemo_sales_order.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
